bf07b917435b17191e300bcd0caf09814f0a4314
John Fajardo <fajardocj@gmail.com>
2019-07-29 21:17:52 -0400

    Adds full CRUD on movies model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,43 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
 
   def index
     @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :genre)
   end
 
 end
