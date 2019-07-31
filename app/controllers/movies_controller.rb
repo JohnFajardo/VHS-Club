5131ff78b35d30a24d734a6901b9c27edbd70bf0
Peter Lin <peterlin5301997@gmail.com>
2019-07-31 13:31:05 -0400

    adds movie poster images

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,44 +1,19 @@
 class MoviesController < ApplicationController
-  before_action :find_movie, only: [:edit, :update, :destroy]
+  before_action :find_movie, only: [:show]
 
   def index
-    @movies = Movie.all
+    @popular_movies = Tmdb::Movie.popular
+    @upcoming_movies = Tmdb::Movie.upcoming
+    @top_rated_movies = Tmdb::Movie.top_rated
   end
 
   def show
-    @movie = Tmdb::Movie.detail(params[:id])
-  end
-
-  def new
-    @movie = Movie.new
-  end
-
-  def create
-    @movie = Movie.create(movie_params)
-    redirect_to @movie
-  end
-
-  def edit
-  end
-
-  def update
-    @movie.update(movie_params)
-    redirect_to @movie
-  end
-
-  def destroy
-    @movie.destroy
-    redirect_to movies_path
   end
 
   private
 
   def find_movie
-    @movie = Movie.find(params[:id])
-  end
-
-  def movie_params
-    params.require(:movie).permit(:title, :year, :genre)
+    @movie = Tmdb::Movie.detail(params[:id])
   end
 
 end
