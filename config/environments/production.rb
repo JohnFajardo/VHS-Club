86a8924398a595a802fcca9fdde5eb2d29849c38
ThatJohn <fajardocj@gmail.com>
2020-01-10 12:58:29 -0500

    Swtiches from SQLite to Postgres

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # Access to TheMoviesDataBase(Tmdb) API
+  Tmdb::Api.key(ENV['tmdb_api_key'])
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
